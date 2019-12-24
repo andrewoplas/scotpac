f80562b9a9f4b7845b91870341352d6e73bc53cd
Andrew Oplas <andrewmoplas@gmail.com>
2019-12-24 22:29:55 +0800

    Refactor classes and added comments

--- js/main.js
+++ js/main.js
@@ -1,22 +1,22 @@
 $(document).ready(function() {
   // Tabs Desktop
   $('.business-tabs-item').click(function() {
-    $('.feature_3 .business-tabs-item')
+    $('.tabs_section .business-tabs-item')
       .parent()
       .removeClass('active');
-    $('.feature_3 .business-tabs-content').removeClass('active');
+    $('.tabs_section .business-tabs-content').removeClass('active');
     $(this)
       .parent()
       .addClass('active');
 
     const contentId = $(this).attr('data-content');
-    $('.feature_3 #' + contentId).addClass('active');
+    $('.tabs_section #' + contentId).addClass('active');
   });
   $('[data-content="business-size"]').click();
 
   // Tabs Mobile
   $('.business-select').change(function() {
-    $('.feature_3_xs .business-tabs-content').removeClass('active');
+    $('.tabs_section_xs .business-tabs-content').removeClass('active');
     const contentId = $(this).val();
     $('#' + contentId).addClass('active');
   });
